da1407804e5929d35a27876901f68b1707d6a6b3
Leonid <leonid0991@gmail.com>
2022-09-10 15:05:08 +0300

    change information by popup form

--- script/script.js
+++ script/script.js
@@ -8,3 +8,39 @@ function togglePopup() {
 
 closeBtn.addEventListener("click", togglePopup);
 editBtn.addEventListener("click", togglePopup);
+
+// Let's find the form in the DOM
+let formElement = document.querySelector(".form"); // Use the querySelector() method
+
+// Next is the form submit handler, though
+// it won't submit anywhere just yet
+
+// Note that the function name starts with a verb
+// and describes exactly what the function does
+function handleProfileFormSubmit(evt) {
+  // This line stops the browser from
+  // submitting the form in the default way.
+  evt.preventDefault();
+  // Having done so, we can define our own way of submitting the form.
+  // We'll explain it in more detail later.
+
+  // Let's find the form fields in the DOM
+  let nameInput = document.querySelector(".form-name"); // Use querySelector()
+  let jobInput = document.querySelector(".form-job"); // Use querySelector()
+
+  // Get the values of each field from the corresponding value property
+
+  // Select elements where the field values will be entered
+
+  // Insert new values using the textContent
+  // property of the querySelector() method
+	let profileName = document.querySelector('.profile__title');
+	let profileJob = document.querySelector('.profile__subtitle');
+	profileName.textContent = nameInput.value;
+	profileJob.textContent = jobInput.value;
+	togglePopup();
+}
+
+// Connect the handler to the form:
+// it will watch the submit event
+formElement.addEventListener("submit", handleProfileFormSubmit);
