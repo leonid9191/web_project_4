ef9db61e263f9ad08a33ac61cf608361e4f364ec
Leonid <leonid0991@gmail.com>
2022-09-13 21:26:46 +0300

    fix popup function

--- script/script.js
+++ script/script.js
@@ -1,18 +1,23 @@
 let closeBtn = document.querySelector(".popup__close");
 let editBtn = document.querySelector(".profile__button-edit");
 let likeBtn = document.querySelector(".card__button-like");
+let popup = document.querySelector(".popup");
+let nameInput = document.querySelector(".form__input_content_name"); // Use querySelector()
+let jobInput = document.querySelector(".form__input_content_job"); // Use querySelector()
+let profileName = document.querySelector(".profile__title");
+let profileJob = document.querySelector(".profile__subtitle");
 
-function togglePopup() {
-  let popup = document.querySelector(".popup");
-  popup.classList.toggle("popup_opened");
+function addPopup() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  popup.classList.add("popup_opened");
 }
-function toggleLike() {
-  likeBtn.classList.toggle("card__button-like-active");
+function removePopup() {
+  popup.classList.remove("popup_opened");
 }
 
-closeBtn.addEventListener("click", togglePopup);
-editBtn.addEventListener("click", togglePopup);
-likeBtn.addEventListener("click", toggleLike);
+closeBtn.addEventListener("click", removePopup);
+editBtn.addEventListener("click", addPopup);
 
 // Let's find the form in the DOM
 let formElement = document.querySelector(".form"); // Use the querySelector() method
@@ -29,21 +34,15 @@ function handleProfileFormSubmit(evt) {
   // Having done so, we can define our own way of submitting the form.
   // We'll explain it in more detail later.
 
-  // Let's find the form fields in the DOM
-  let nameInput = document.querySelector(".form__input_content_name"); // Use querySelector()
-  let jobInput = document.querySelector(".form__input_content_job"); // Use querySelector()
-
   // Get the values of each field from the corresponding value property
 
   // Select elements where the field values will be entered
 
   // Insert new values using the textContent
   // property of the querySelector() method
-	let profileName = document.querySelector('.profile__title');
-	let profileJob = document.querySelector('.profile__subtitle');
-	profileName.textContent = nameInput.value;
-	profileJob.textContent = jobInput.value;
-	togglePopup();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  removePopup();
 }
 
 // Connect the handler to the form:
