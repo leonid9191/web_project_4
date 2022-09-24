d27a33c1cd5b9bd1bb0bfa630f8c461e4a2a3459
Leonid <leonid0991@gmail.com>
2022-09-24 20:16:08 +0300

    add cards via js

--- script/script.js
+++ script/script.js
@@ -1,11 +1,41 @@
-let closeBtn = document.querySelector(".popup__close");
-let editBtn = document.querySelector(".profile__button-edit");
-let likeBtn = document.querySelector(".card__button-like");
-let popup = document.querySelector(".popup");
-let nameInput = document.querySelector(".form__input_content_name"); // Use querySelector()
-let jobInput = document.querySelector(".form__input_content_job"); // Use querySelector()
-let profileName = document.querySelector(".profile__title");
-let profileJob = document.querySelector(".profile__subtitle");
+const closeBtn = document.querySelector(".popup__close");
+const editBtn = document.querySelector(".profile__button-edit");
+const likeBtn = document.querySelector(".card__button-like");
+const popup = document.querySelector(".popup");
+const nameInput = document.querySelector(".form__input_content_name");
+const jobInput = document.querySelector(".form__input_content_job");
+const profileName = document.querySelector(".profile__title");
+const profileJob = document.querySelector(".profile__subtitle");
+const formElement = document.querySelector(".form");
+const gallery = document.querySelector(".gallery");
+const cardTemplate = document.querySelector("#card").content;
+
+const initialCards = [
+  {
+    name: "Government office in Putrajaya, Malaysia.",
+    link: "https://images.unsplash.com/photo-1648291881755-f984c18e16cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fA%3D%3D&w=1000&q=80",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
 
 function addPopup() {
   nameInput.value = profileName.textContent;
@@ -19,32 +49,22 @@ function removePopup() {
 closeBtn.addEventListener("click", removePopup);
 editBtn.addEventListener("click", addPopup);
 
-// Let's find the form in the DOM
-let formElement = document.querySelector(".form"); // Use the querySelector() method
 
-// Next is the form submit handler, though
-// it won't submit anywhere just yet
-
-// Note that the function name starts with a verb
-// and describes exactly what the function does
 function handleProfileFormSubmit(evt) {
   // This line stops the browser from
   // submitting the form in the default way.
   evt.preventDefault();
-  // Having done so, we can define our own way of submitting the form.
-  // We'll explain it in more detail later.
-
-  // Get the values of each field from the corresponding value property
-
-  // Select elements where the field values will be entered
-
-  // Insert new values using the textContent
-  // property of the querySelector() method
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   removePopup();
 }
 
-// Connect the handler to the form:
-// it will watch the submit event
+//Add all cards from array by templates
+initialCards.forEach((card) => {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__image").src = card.link;
+  cardElement.querySelector(".card__title").textContent = card.name;
+  gallery.append(cardElement);
+});
+
 formElement.addEventListener("submit", handleProfileFormSubmit);
