32c56a54352a920531ae5e14013ffd1441fcc649
Leonid <leonid0991@gmail.com>
2022-10-19 21:03:45 +0300

    validation fields with red line and comment

--- script/script.js
+++ script/script.js
@@ -22,6 +22,7 @@ const addcardModalCloseButton = addCardModal.querySelector(".popup__close");
 const nameEditProfileInput = document.querySelector(
   ".form__input_content_name"
 );
+
 const jobEditProfileInput = document.querySelector(".form__input_content_job");
 
 const titleAddCardInput = document.querySelector(".form__input_content_title");
@@ -159,3 +160,71 @@ initialCards.forEach((card) => {
 
 editFormElement.addEventListener("submit", handleProfileFormSubmit);
 addCardFormElement.addEventListener("submit", handleAddCardFormSubmit);
+
+
+
+//Validation
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  // Find the error message element inside the very function
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  // The rest remains unchanged
+  inputElement.classList.add("form__input_type_error");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("form__input-error_active");
+};
+
+const hideInputError = (formElement, inputElement) => {
+  // Find the error message element
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  // The rest remains unchanged
+  inputElement.classList.remove("form__input_type_error");
+  errorElement.classList.remove("form__input-error_active");
+  errorElement.textContent = "";
+}; 
+
+// Code the 3rd function, which checks if the field is valid
+const isValid = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    // If NOT (!), show the error element
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    // If it's valid, hide the error element
+    hideInputError(formElement, inputElement);
+  }
+};
+const setEventListeners = (formElement) => {
+  // Find all fields inside the form, and
+  // make an array from them using the Array.from() method
+  const inputList = Array.from(formElement.querySelectorAll(".form__input"));
+
+  // Iterate over the resulting array
+  inputList.forEach((inputElement) => {
+    // add the input event handler to each field
+    inputElement.addEventListener("input", () => {
+      // Call the isValid() function inside the callback,
+      // and pass the form and the element to be checked to it
+      isValid(formElement, inputElement)
+    });
+  });
+};
+const enableValidation = () => {
+  // It will find all forms with the specified class in DOM, and
+  // make an array from them using the Array.from() method
+  const formList = Array.from(document.querySelectorAll(".form"));
+
+  // Iterate over the resulting array
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      // Cancel default behavior for each form
+      evt.preventDefault();
+    });
+
+    // Call the setEventListeners() function for each form,
+    // taking a form element as an argument
+    setEventListeners(formElement);
+  });
+};
+
+// Call the function
+enableValidation();
